ddd2c9c27a720ec9b4122050703b2da858c8bb85
Escaflownevan <30783440+Escaflownevan@users.noreply.github.com>
2018-05-08 12:27:59 +0200

    v2.0.4.1
    -Fixed semantic bug

---
+++ Coins-linux-x64/resources/app/main.js
@@ -0,0 +1,130 @@
+const electron = require("electron");
+// Module to control application life.
+const app = electron.app;
+const windowStateKeeper = require("electron-window-state");
+let win;
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent(app)) {
+	// squirrel event handled and app will exit in 1000ms, so don't do anything else
+	return;
+}
+
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const url = require("url");
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+app.on("ready", function() {
+	// Load the previous state with fallback to defaults
+	let mainWindowState = windowStateKeeper({
+		defaultWidth: 500,
+		defaultHeight: 750
+	});
+
+	// Create the window using the state information
+	win = new BrowserWindow({
+		x: mainWindowState.x,
+		y: mainWindowState.y,
+		width: mainWindowState.width,
+		height: mainWindowState.height
+	});
+	win.loadURL(
+		url.format({
+			pathname: path.join(__dirname, "index.html"),
+			protocol: "file:",
+			slashes: true
+		})
+	);
+	// Let us register listeners on the window, so we can update the state
+	// automatically (the listeners will be removed when the window is closed)
+	// and restore the maximized or full screen state
+	mainWindowState.manage(win);
+});
+// Quit when all windows are closed.
+app.on("window-all-closed", function() {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", function() {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+function handleSquirrelEvent(application) {
+	if (process.argv.length === 1) {
+		return false;
+	}
+
+	const ChildProcess = require("child_process");
+	const path = require("path");
+
+	const appFolder = path.resolve(process.execPath, "..");
+	const rootAtomFolder = path.resolve(appFolder, "..");
+	const updateDotExe = path.resolve(path.join(rootAtomFolder, "Update.exe"));
+	const exeName = path.basename(process.execPath);
+
+	const spawn = function(command, args) {
+		let spawnedProcess, error;
+
+		try {
+			spawnedProcess = ChildProcess.spawn(command, args, {
+				detached: true
+			});
+		} catch (error) {}
+
+		return spawnedProcess;
+	};
+
+	const spawnUpdate = function(args) {
+		return spawn(updateDotExe, args);
+	};
+
+	const squirrelEvent = process.argv[1];
+	switch (squirrelEvent) {
+		case "--squirrel-install":
+		case "--squirrel-updated":
+			// Optionally do things such as:
+			// - Add your .exe to the PATH
+			// - Write to the registry for things like file associations and
+			//   explorer context menus
+
+			// Install desktop and start menu shortcuts
+			spawnUpdate(["--createShortcut", exeName]);
+
+			setTimeout(application.quit, 1000);
+			return true;
+
+		case "--squirrel-uninstall":
+			// Undo anything you did in the --squirrel-install and
+			// --squirrel-updated handlers
+
+			// Remove desktop and start menu shortcuts
+			spawnUpdate(["--removeShortcut", exeName]);
+
+			setTimeout(application.quit, 1000);
+			return true;
+
+		case "--squirrel-obsolete":
+			// This is called on the outgoing version of your app before
+			// we update to the new version - it's the opposite of
+			// --squirrel-updated
+
+			application.quit();
+			return true;
+	}
+}
